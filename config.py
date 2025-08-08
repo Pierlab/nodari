acc75c16fd40f34f7fae00f0d579a41e085151c5
Pierlab <plab71@hotmail.com>
2025-08-08 16:17:47 +0200

    reconfig

--- config.py
+++ config.py
@@ -8,9 +8,9 @@ FONT_SIZE = 18
 
 # Simulation timing
 FPS = 24
-TIME_SCALE = 60  # one simulated minute per real second
-START_TIME = 7 * 3600 + 30 * 60  # 07:30 in seconds
+TIME_SCALE = 600  # one simulated minute per real second
+START_TIME = 8 * 3600 + 0 * 60  # 07:30 in seconds
 
 # Character movement speeds in kilometres per hour
-CHARACTER_SPEED = 5.0
+CHARACTER_SPEED = 1.0
 CHARACTER_RANDOM_SPEED = 2.0
