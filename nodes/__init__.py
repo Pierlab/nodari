b1ba166d453219d19042b71cfd5a47cdfa2d2620
Pierlab <plab71@hotmail.com>
2025-08-18 21:35:27 +0200

    Expose node classes in package

---
+++ nodes/__init__.py
@@ -0,0 +1,12 @@
+"""Expose core node classes and trigger plugin registration."""
+
+from .building import BuildingNode  # noqa: F401
+from .resource import ResourceNode  # noqa: F401
+from .worker import WorkerNode  # noqa: F401
+
+__all__ = [
+    "BuildingNode",
+    "ResourceNode",
+    "WorkerNode",
+]
+
